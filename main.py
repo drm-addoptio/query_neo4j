85c67ff32ee7aa6cb9279ed9624e2fb3ed98f07c
Martin Seeger <ms@addopt.io>
2024-11-25 17:28:04 +0100

    still bugfixing

--- main.py
+++ main.py
@@ -122,7 +122,7 @@ def main(request):
 
 def querykb(cypher: str, user: str) -> list:
     try:
-        with GraphDatabase.driver(uri=URI, auth=AUTH, NotificationMinimumSeverity="OFF") as driver:
+        with GraphDatabase.driver(uri=URI, auth=AUTH, warn_notification_severity="OFF") as driver:
             driver.verify_connectivity()
             logger.info(f"Start querying Neo4j with: {cypher}")
 

56c4c1c2225c1edaf237de09d946d3cc2efa9464
Martin Seeger <ms@addopt.io>
2024-11-26 14:10:54 +0100

    added relationship types

--- config/styles_config.py
+++ config/styles_config.py
@@ -28,5 +28,65 @@ relationship_style_config = {
         "caption": "MENTIONS",
         "styles": [],
         "color": "black"
+    },
+    "AFFECTS": {
+        "caption": "AFFECTS",
+        "styles": [],
+        "color": "black"
+    },
+    "CITED": {
+        "caption": "CITED",
+        "styles": [],
+        "color": "black"
+    },
+    "HAS_AFFECTED_PRODUCTS": {
+        "caption": "HAS_AFFECTED_PRODUCTS",
+        "styles": [],
+        "color": "black"
+    },
+    "HAS_CPE": {
+        "caption": "HAS_CPE",
+        "styles": [],
+        "color": "black"
+    },
+    "HAS_IMPACT": {
+        "caption": "HAS_IMPACT",
+        "styles": [],
+        "color": "black"
+    },
+    "HAS_PROBLEM_TYPE": {
+        "caption": "HAS_PROBLEM_TYPE",
+        "styles": [],
+        "color": "black"
+    },
+    "HAS_VENDOR": {
+        "caption": "HAS_VENDOR",
+        "styles": [],
+        "color": "black"
+    },
+    "METIONS": {
+        "caption": "MENTIONS",
+        "styles": [],
+        "color": "black"
+    },
+    "NEW_VERSION": {
+        "caption": "NEW_VERSION",
+        "styles": [],
+        "color": "black"
+    },
+    "SUB_TECHNIQUE_OF": {
+        "caption": "SUB_TECHNIQUE_OF",
+        "styles": [],
+        "color": "black"
+    },
+    "USES": {
+        "caption": "USES",
+        "styles": [],
+        "color": "black"
+    },
+    "SUB_TECHNIQUE_OF": {
+        "caption": "SUB_TECHNIQUE_OF",
+        "styles": [],
+        "color": "black"
     }
 }
